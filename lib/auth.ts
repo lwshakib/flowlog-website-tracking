4a8820a6c0b090b51f9363247d8318420d7e54b0
Shakib Khan <leadwithshakib@gmail.com>
2026-01-01 12:37:43 +0600

    feat: Add new UI components for logo, hero, and header, implement BetterAuth for authentication, and integrate motion for animations.

--- lib/auth.ts
+++ lib/auth.ts
@@ -14,13 +14,7 @@ export const auth = betterAuth({
       enabled: true,
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    },
-    github: {
-      enabled: true,
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-      scope:["repo"]
-    },
+    }
   },
   account: {
     accountLinking: {
